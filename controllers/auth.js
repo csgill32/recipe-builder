a03916232b1454bc659d24b306458e1ff69659f0
chanlathung1 <chanlathung@gmail.com>
2020-06-02 15:40:43 -0700

    adds post route for creating user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const db = require("../models");
 
 
 // root route /
@@ -10,6 +12,21 @@ router.get("/register", function(req, res) {
 });
 
 // register post
+router.post("/register", async function (req, res) {
+    try {
+      const foundUser = await db.User.findOne({ email: req.body.email });
+      if (foundUser) {
+        return res.send({ message: "Account is already registered" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(req.body.password, salt);
+      req.body.password = hash;
+      const newUser = await db.User.create(req.body);
+      res.redirect("/login");
+    } catch (err) {
+      res.send({ message: "Internal Server Error", error: err });
+    }
+  });
 
   // login form
   router.get("/login", function (req, res) {
