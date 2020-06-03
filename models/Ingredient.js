08c4cbd1c39c18679f8ed0c8545ba306a193ff5d
chanlathung1 <chanlathung@gmail.com>
2020-06-03 10:28:19 -0700

    update model

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
 
 const ingredientSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    name: { type: String },
-    quantity: { type: Number },
-=======
     name: { type: String, required: true },
     quantity: { type: String },
->>>>>>> submaster
     measurement: { type: String },
     recipe: {
         type: mongoose.Schema.Types.ObjectId,
